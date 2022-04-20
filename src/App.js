05686769463fcf8ab3fddd286417d049ae599212
Orlando Castillo <orlandocastillo@OCastillo-c02f22yzq05p.local>
2022-04-20 08:59:40 -0400

    added aws auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'; // default theme
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
-}
-
+    <Authenticator>
+      {({ user, signOut }) => {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1>Hello from V2</h1>
+            </header>
+            <button onClick={signOut}>Sign out</button>
+          </div>
+        );
+      }
+    }
+    </Authenticator>
+  )
+  }
+  
 export default App;
